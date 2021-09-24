8424518f020427324a3f1ccfaf414decd338b361
Carlos Felipe Araújo <carlosxfelipe@gmail.com>
2021-09-24 20:58:57 -0300

    wip: exibindo redacao

--- src/pages/Redacao/index.js
+++ src/pages/Redacao/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // import { Container } from './styles';
 
-const Redacao = () => {
+const Redacao = ({ route }) => {
+  const { redacaoId } = route.params;
+  const [redacao, setRedacao] = useState(null);
+
+  useEffect(async () => {
+    const token = await AsyncStorage.getItem('@access_token');
+    const response = await axios.get(
+      `https://desafio.pontue.com.br/redacao/${redacaoId}`,
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+    if (redacao === null) {
+      setRedacao(response.data.data);
+    }
+  }, [redacao]);
+
   return <View />;
 };
 
